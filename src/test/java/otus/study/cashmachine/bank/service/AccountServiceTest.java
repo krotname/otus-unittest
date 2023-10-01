c2d452cd086656940a3a11379c4571341d9f2661
Ovcharenko.AA <Ovcharenko.AA@esphere.ru>
2023-10-01 22:12:42 +0300

    dz unit tests ready

--- src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
+++ src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
@@ -2,24 +2,39 @@ package otus.study.cashmachine.bank.service;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.internal.matchers.Null;
+import org.mockito.junit.jupiter.MockitoExtension;
 import otus.study.cashmachine.bank.dao.AccountDao;
+import otus.study.cashmachine.bank.data.Account;
 import otus.study.cashmachine.bank.service.impl.AccountServiceImpl;
 
+import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+@ExtendWith(MockitoExtension.class)
 public class AccountServiceTest {
 
-    AccountDao accountDao;
+    @Mock
+    private AccountDao accountDao;
 
-    AccountServiceImpl accountServiceImpl;
+    @Spy
+    @InjectMocks
+    private AccountServiceImpl accountServiceImpl;
 
     @Test
     void createAccountMock() {
-// @TODO test account creation with mock and ArgumentMatcher
+        Account account = accountServiceImpl.createAccount(ArgumentMatchers.any());
+        System.out.println(account);
     }
 
     @Test
     void createAccountCaptor() {
-//  @TODO test account creation with ArgumentCaptor
+        Account account = accountServiceImpl.createAccount(ArgumentCaptor.forClass(BigDecimal.class).capture());
+        assertNull(account);
     }
 
     @Test
--- src/test/java/otus/study/cashmachine/machine/service/CashMachineServiceTest.java
+++ src/test/java/otus/study/cashmachine/machine/service/CashMachineServiceTest.java
@@ -5,15 +5,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import otus.study.cashmachine.TestUtil;
 import otus.study.cashmachine.bank.dao.CardsDao;
+import otus.study.cashmachine.bank.data.Card;
 import otus.study.cashmachine.bank.service.AccountService;
 import otus.study.cashmachine.bank.service.impl.CardServiceImpl;
 import otus.study.cashmachine.machine.data.CashMachine;
 import otus.study.cashmachine.machine.data.MoneyBox;
 import otus.study.cashmachine.machine.service.impl.CashMachineServiceImpl;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class CashMachineServiceTest {
+
+    @Captor
+    private ArgumentCaptor<String> captor;
 
     @Spy
     @InjectMocks
@@ -40,7 +53,12 @@ class CashMachineServiceTest {
 
     @Test
     void getMoney() {
-// @TODO create get money test using spy as mock
+        when(cardsDao.getCardByNumber("2222"))
+                .thenReturn(new Card(1, "2222", 2L, TestUtil.getHash("1234")));
+
+        List<Integer> money = cashMachineService.getMoney(cashMachine, "2222", "1234", BigDecimal.valueOf(150));
+
+        assertTrue(money.isEmpty());
     }
 
     @Test
@@ -54,11 +72,17 @@ class CashMachineServiceTest {
 
     @Test
     void changePin() {
-// @TODO create change pin test using spy as implementation and ArgumentCaptor and thenReturn
+        when(cardsDao.getCardByNumber(captor.capture()))
+                .thenReturn(new Card(1,"1234", 1L, TestUtil.getHash("2222")));
+
+        assertTrue(cashMachineService.changePin("1234", "2222", "1111"));
     }
 
     @Test
     void changePinWithAnswer() {
-// @TODO create change pin test using spy as implementation and mock an thenAnswer
+        when(cardsDao.getCardByNumber(captor.capture()))
+                .thenAnswer(i -> new Card(1, String.valueOf(i.getArguments()[0]), 1L, TestUtil.getHash("2222")));
+
+        assertTrue(cashMachineService.changePin("1234", "2222", "1111"));
     }
 }
